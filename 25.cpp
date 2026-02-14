fa53816a78ea6c490c57eef6d219fe1e29166fc5
trigo <ajitesh295@gmail.com>
2026-02-14 21:35:59 +0530

    Enhance password validation checks
    Updated password validation logic to check for uppercase, lowercase, digit, and special characters.

--- 25.cpp
+++ 25.cpp
@@ -1,13 +1,24 @@
 //Question 25 lab manual
 #include <iostream>
 using namespace std;
+
 int main(){
-    string pass;
-    cout << "enter your password:";
-    getline(cin,pass);
-    for(int i=0;pass[i]!='\0';i++){
-        if(pass[i]>='a' && pass[i]<='z'){
-        
-        }
-    }   
+    string password;
+    bool upper = false, lower = false, digit = false,special = false;
+    cout <<"enter password:";
+    cin >>password;
+    for(int i=0; i<password.length();i++){
+        char ch = password[i];
+        if(ch>='A' && ch<='Z')
+            upper=true;
+        else if(ch>='a' && ch<='z')
+            lower=true;
+        else if(ch>='0' && ch<='9')
+            digit=true;
+        else if(ch=='@' || ch=='#' || ch=='$' || ch=='%' || ch=='!' || ch=='&' || ch=='*' || ch=='/' || ch==':' || ch==';' )
+            special=true;
+    }
+    if(upper && lower && digit && special) cout << "Password is VALID";
+    else cout << "Password is INVALID";
+    return 0;
 }
