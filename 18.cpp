3ac52092c535ae21a608a77dbca815851e4a7c00
trigo <ajitesh295@gmail.com>
2026-02-23 23:57:59 +0530

    Add files via upload

---
+++ 18.cpp
@@ -0,0 +1,22 @@
+// Question 18 lab manual
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string id;
+    cout<<"enter ID: ";
+    cin>>id;
+    bool check= true;
+    int st=0;
+    int end=id.length()-1;
+    while (st<end) {
+        if (id[st] !=id[end]) {
+            check=false;
+            break;
+        }
+        st++,end--;
+    }
+    if (check) cout<<"ID is a palindrome.";
+    else cout<<"ID is NOT a palindrome.";
+}
